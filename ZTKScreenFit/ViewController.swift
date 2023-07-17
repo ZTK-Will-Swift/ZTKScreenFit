1ea8a08127ec113b314d382b61f0d92fc86c78fc
will <1753552195@qq.com>
2023-07-17 20:49:08 +0800

    Initial Commit

---
+++ ZTKScreenFit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZTKScreenFit
+//
+//  Created by 张天康 on 2023/7/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
