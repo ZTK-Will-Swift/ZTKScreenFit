305f6e144273d6e11b639be2e917f6eb453a239e
will <1753552195@qq.com>
2023-07-26 00:33:01 +0800

    添加部分备注

--- ZTKScreenFit/ViewController.swift
+++ ZTKScreenFit/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         showViewWidth.constant = 300.pt
+        
+        //iPhone 6 = 300
+        //iPhone 14 = 312.0
+        //iPhone 14 Pro = 314.4
+        //iPhone 14 Plus = 342.4
+        //iPhone 14 Pro Max = 344.0
         widthLabel.text = "\(300.pt)"
     }
 
